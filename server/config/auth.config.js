c7d11fd46e468689f76cc429ebc2e1f7d151b21d
Cezar Mocan <mocancezar@gmail.com>
2021-09-02 19:52:51 -0400

    server routes & middlewares

---
+++ server/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "dwc-secret-key-temp"
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dbConfig = require("./config/db.config.js")
+const db = require("./models");
+const Role = db.role;
 
 const app = express();
 
@@ -16,10 +19,45 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initializeDB();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+function initializeDB() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({ name: "user" }).save(err => {
+        if (err) { console.log("error", err); }
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({ name: "admin" }).save(err => {
+        if (err) { console.log("error", err); }
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
+  
+
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Test." });
 });
+
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
