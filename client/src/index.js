fe47ab570ac7789ca3907d3e107c2bda6c98cff7
Cezar Mocan <mocancezar@gmail.com>
2021-09-02 21:19:55 -0400

    Removed default react stuff

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,16 @@ import App from './App';
 import store from './store'
 import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals';
+import { Helmet } from 'react-helmet'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>DWC v2</title>
+        <link rel="icon" type="image/png" href="favicon.ico" sizes="16x16" />
+      </Helmet>
       <App />
     </Provider>
   </React.StrictMode>,
