c7d14a4f9e30b23bdd850a99fe103a67b5c75b59
sosunnyproject <parksosun1103@gmail.com>
2021-09-16 23:54:48 +0900

    'test-pixijs'

---
+++ canvas/index.js
@@ -0,0 +1,36 @@
+// https://pixijs.io/guides/basics/getting-started.html
+// https://www.html5gamedevs.com/topic/45444-unable-to-load-pixijs-as-a-module/
+// https://codesandbox.io/s/app-architecture-3-t6cfv?file=/src/models.js
+// import * as PIXI from "pixi.js";
+
+// The application will create a renderer using WebGL, if possible,
+// with a fallback to a canvas render. It will also setup the ticker
+// and the root stage PIXI.Container
+const app = new PIXI.Application();
+
+// The application will create a canvas element for you that you
+// can then insert into the DOM
+document.body.appendChild(app.view);
+
+// load the texture we need
+app.loader.add('bunny', 'bunny.jpg').load((loader, resources) => {
+    // This creates a texture from a 'bunny.png' image
+    const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+    // Setup the position of the bunny
+    bunny.x = app.renderer.width / 2;
+    bunny.y = app.renderer.height / 2;
+
+    // Rotate around the center
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+
+    // Add the bunny to the scene we are building
+    app.stage.addChild(bunny);
+
+    // Listen for frame updates
+    app.ticker.add(() => {
+         // each frame we spin the bunny around a bit
+        bunny.rotation += 0.01;
+    });
+});
