773a15ef8f1c455186bf0b67c110ea9ef1efc908
Cezar Mocan <mocancezar@gmail.com>
2021-09-18 18:10:55 -0400

    Changed ports

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ require('./routes/user.routes')(app);
 require('./routes/socket.routes')(io)
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
