8383680015f4fbf7accb08930f73b6e1a9453ad0
sosunnyproject <parksosun1103@gmail.com>
2021-09-20 00:21:39 +0900

    fix

--- canvas/app.js
+++ canvas/app.js
@@ -2,12 +2,9 @@ import LOGIN from './src/html/login.js';
 import SIGNUP from './src/html/signup.js';
 import USER from './src/html/user.js';
 import CANVAS from './src/html/canvas.js';
-
-import UserService from './src/services/user.service';
 import AuthService from './src/services/auth.service';
 
 export const LOGGEDIN = localStorage.getItem("user") ? true: false;
-export const USERNAME = JSON.parse(localStorage.getItem("user"))?.username;
 
 export const ROUTES = {
  '/signup': SIGNUP,
@@ -21,10 +18,9 @@ const userDiv = document.getElementById("sub");
 userDiv.innerHTML = ROUTES[uri];
 const canvasDiv = document.getElementById("root");
 
-// hide canvas div if not loggedin
+// hide div depends on uri
 if(!LOGGEDIN || (uri !== '/')) {
   canvasDiv.style.display = "none";
-  // window.location.replace(window.location.origin)
 } else if(LOGGEDIN && (uri === '/')) {
   userDiv.style.display = "none";
 }
@@ -54,14 +50,14 @@ window.submitSignup = (event) => {
  AuthService.register(username, email, password);
 }
 
-// button - redirect
+// logo button - redirect
 window.onClickLogoBtn = () => {
-  console.log('logo')
-  console.log(uri)
-  if(uri === '/user') {
-    window.location.replace(origin + '/');
-  } else if(uri === '/') {
-    window.location.replace(origin + '/user');
+  if(LOGGEDIN) {
+    if(uri === '/user') {
+      window.location.replace(origin + '/');
+    } else if(uri === '/') {
+      window.location.replace(origin + '/user');
+    }
   }
 }
 
