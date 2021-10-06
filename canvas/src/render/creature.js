ba0dd587bb9d2add1c7c020cd8fb9f2bc62df704
Cezar Mocan <mocancezar@gmail.com>
2021-10-06 11:40:34 -0400

    movement and shape morphing functional

--- canvas/src/render/creature.js
+++ canvas/src/render/creature.js
@@ -3,7 +3,7 @@ import { distanceAndAngleBetweenTwoPoints, Vector } from './utils';
 import { DWC_META } from '../../../shared-constants';
 import PixiSVG from '../svg-lib'
 import SVGShape from './Geometry/SVGCreatureShape';
-import { randomElementFromArray, easeInOutBounce } from './utils';
+import { randomElementFromArray, easeInOutBounce, easeInOutQuart, lerp } from './utils';
 
 export default class Creature extends PIXI.Graphics {
     constructor(state) {
@@ -23,19 +23,21 @@ export default class Creature extends PIXI.Graphics {
         this.toCreatureType = this.animatedProperties.shape.to
         this.shapeMorphDuration = this.animatedProperties.shape.duration
         this.shapeMorphAlpha = 0
-        this.shapeMorphAlphaSgn = 1
 
         let fromX = this.animatedProperties.position.from.x
         let fromY = this.animatedProperties.position.from.y
         let toX = this.animatedProperties.position.to.x
-        let toY = this.animatedProperties.position.to.y
+        let toY = this.animatedProperties.position.to.y        
 
         // This should actually be somewhere between from and to, depending on the timestamp.
         this.x = fromX
         this.y = fromY
         this.vx = 0
         this.vy = 0
+        this.originPos = { x: fromX, y: fromY }
         this.target = { x: toX, y: toY }
+        this.movementDuration = this.animatedProperties.position.duration
+        this.movementAlpha = 0
 
         const svgData = PIXI.Loader.shared.resources[this.creatureType].data
 
@@ -54,8 +56,6 @@ export default class Creature extends PIXI.Graphics {
         this.destinationMarker.x = toX - this.x
         this.destinationMarker.y = toY - this.y
         this.addChild(this.destinationMarker)
-
-        this.totalTime = 0
     }
 
     updateState(newState) {
@@ -82,6 +82,10 @@ export default class Creature extends PIXI.Graphics {
         this.target.y = prop.to.y
         this.destinationMarker.x = this.target.x
         this.destinationMarker.y = this.target.y
+        this.originPos.x = this.x
+        this.originPos.y = this.y
+        this.movementAlpha = 0
+        this.movementDuration = this.animatedProperties.position.duration
     }
 
     updateTargetShape(prop) {
@@ -91,33 +95,27 @@ export default class Creature extends PIXI.Graphics {
         this.shapeMorphDuration = prop.duration
     }
 
-    tick(delta) {
-        // Movement of the entire creature
-        this.totalTime += delta
+    tick(d) {
+        const delta = PIXI.Ticker.shared.elapsedMS
 
-        const target = this.target
-
-        let lastStep = 0;
-        let milliseconds = 0;
-        milliseconds += delta;
-        var elapsed = milliseconds - lastStep;
-        lastStep = milliseconds;
-
-        var data = distanceAndAngleBetweenTwoPoints(this.x, this.y, target.x, target.y)
-        var velocity = data.distance / 0.1;
-        var toTargetVector = new Vector(velocity, data.angle)
-        var elapsedSeconds = elapsed / 1000;
-
-        this.x += (toTargetVector.magX * elapsedSeconds)
-        this.y += (toTargetVector.magY * elapsedSeconds)
-
-        this.destinationMarker.x = target.x - this.x
-        this.destinationMarker.y = target.y - this.y
+        this.destinationMarker.x = this.target.x - this.x
+        this.destinationMarker.y = this.target.y - this.y
 
         // Per-frame update for the creature SVG Shape outlines
-        this.svgShape.tick()        
+        this.svgShape.tick()
+
+        // Movement animation
+        if (this.movementAlpha >= 1) {
+
+        } else {
+            const step = delta / (1000 * this.movementDuration)
+            this.movementAlpha += step
+            this.easedMovementAlpha = easeInOutQuart(this.movementAlpha)
+            this.x = lerp(this.originPos.x, this.target.x, this.easedMovementAlpha)
+            this.y = lerp(this.originPos.y, this.target.y, this.easedMovementAlpha)
+        }
         
-        // Shape morphing 
+        // Shape morphing animation
         if (this.shapeMorphAlpha >= 1) {
             // TODO (cezar): Shape morphing should only start when current transition has been completed,
             // otherwise we might see jumps.
@@ -126,6 +124,6 @@ export default class Creature extends PIXI.Graphics {
             this.shapeMorphAlpha += step
             this.svgShape.morph(this.creatureType, this.toCreatureType, easeInOutBounce(this.shapeMorphAlpha))
             // console.log('delta: ', delta, 'step: ', step, ' alpha: ', this.shapeMorphAlpha)
-        }
+        }     
     }
 }
