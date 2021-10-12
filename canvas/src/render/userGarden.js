f627bdc717790f620e6aaed1ba790640c81ac0fa
Cezar Mocan <mocancezar@gmail.com>
2021-10-11 21:58:40 -0400

    comments

--- canvas/src/render/userGarden.js
+++ canvas/src/render/userGarden.js
@@ -126,12 +126,12 @@ function render(app) {
   // (cezar): Example of a gradient shader, if we want to implement the designs.
   const gradientUniforms = {
     u_time: 1.0,
-    u_point1: [0.0, 0.0],
-    u_radius1: 0.1,
-    u_color1: [0.6, 0.2, 0.3],
-    u_point2: [1.0, 1.0],
-    u_radius2: 0.1,
-    u_color2: [0.2, 0.5, 0.8],
+    u_point1: [0.0, 0.0], // first center of the radial gradient, coordinates go from (0, 0) to (1, 1)
+    u_radius1: 0.1, // radius of first point of radial gradient
+    u_color1: [0.6, 0.2, 0.3], // color of first point of radial gradient
+    u_point2: [1.0, 1.0], // second center of the radial gradient, coordinates go from (0, 0) to (1, 1)
+    u_radius2: 0.1, // radius of second point of radial gradient
+    u_color2: [0.2, 0.5, 0.8], // color of second point of radial gradient
     u_resolution: [WIDTH * 1.0, HEIGHT * 1.0]
   }
   const gradientShader = PIXI.Shader.from(vertex, gradientFragment, gradientUniforms);
