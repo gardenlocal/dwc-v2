784ba095dde5476388fb1ac546ab92af129656f4
Cezar Mocan <mocancezar@gmail.com>
2021-10-18 09:04:16 -0400

    tests

--- canvas/src/render/Geometry/SVGCreatureLayer.js
+++ canvas/src/render/Geometry/SVGCreatureLayer.js
@@ -1,6 +1,6 @@
 import * as PIXI from 'pixi.js'
 import * as geometric from 'geometric'
-import { distanceAndAngleBetweenTwoPoints } from '../utils'
+import { distanceAndAngleBetweenTwoPoints, randomInRange } from '../utils'
 
 const morphOffsetCache = {}
 
@@ -16,7 +16,18 @@ export default class SVGLayer extends PIXI.Graphics {
         // For now, we can deal with simple shapes.
         this.points = this.svgObj.geometry.graphicsData[0].points        
         this.points = this.resampleByPoints(pointCount)
-        this.draw()
+
+        this.pRandom = []
+        for (let i = 0; i < this.points.length; i++) {
+            this.pRandom.push({
+                posAlpha: randomInRange(0.3, 0.7),
+                angle: randomInRange(Math.PI / 2 - 0.3, Math.PI / 2 + 0.3),
+                r1Factor: randomInRange(0.1, 0.6),
+                r2Factor: randomInRange(0.1, 0.6)
+            })
+        }
+
+        this.draw()        
     }
 
     toGeometricPoly(p) {
@@ -121,7 +132,9 @@ export default class SVGLayer extends PIXI.Graphics {
         let newPoints = []
 
         for (let i = 0; i < this.points.length; i += 2) {
-            let dst = distanceAndAngleBetweenTwoPoints(this.points[i], this.points[i + 1], this.points[i + 2], this.points[i + 3]).distance
+            let pC = [this.points[i], this.points[i + 1]]
+            let pN = (i < this.points.length - 2) ? ([this.points[i + 2], this.points[i + 3]]) : ([this.points[0], this.points[1]])
+            let dst = distanceAndAngleBetweenTwoPoints(pC[0], pC[1], pN[0], pN[1]).distance
             if (totalDist + dst > targetDist) {
                 let interpAlphas = []
                 while (totalDist + dst > targetDist) {
@@ -133,7 +146,7 @@ export default class SVGLayer extends PIXI.Graphics {
                 totalDist = dst
 
                 interpAlphas.forEach(a => {
-                    let newPoint = geometric.lineInterpolate([[this.points[i], this.points[i + 1]], [this.points[i + 2], this.points[i + 3]]])(a)
+                    let newPoint = geometric.lineInterpolate([pC, pN])(a)
                     newPoints.push(newPoint[0], newPoint[1])
                 })
             } else {
@@ -146,7 +159,7 @@ export default class SVGLayer extends PIXI.Graphics {
 
     resampleByPoints(noPoints) {
         const p = this.calculatePerimeter()
-        return this.resampleByDistance(p / noPoints)
+        return this.resampleByDistance((p - 0.001) / noPoints)
     }
 
     calculatePerimeter() {
@@ -154,14 +167,166 @@ export default class SVGLayer extends PIXI.Graphics {
     }
 
     draw() {
+
+
         this.clear()
+        this.drawFillAndStroke()
+        // this.drawStar()
+        // this.drawSquiggleGeneral()
+        // this.drawSquiggleOutline()
+        this.drawBubblyOutline()
         // this.setStyle(this.svgObj.__style, this.svgObj.__matrix)
-        this.beginFill(0xfafafa)
-        this.lineStyle(10, 0x2a2a2a, 1)
-        this.drawPolygon(this.points)        
-        this.endFill()
 
         //this.drawDebug()
+    }
+
+    drawFillAndStroke() {
+        // this.beginFill(0xfafafa)
+        this.lineStyle(3, 0x2a2a2a, 1)
+        this.drawPolygon(this.points)        
+        // this.endFill()
+    }
+
+    drawStar() {    
+        // if (!this.points50) {
+            this.points50 = this.resampleByPoints(100)
+        // }
+        const p = this.points50
+        const center = geometric.polygonCentroid(this.toGeometricPoly(p))
+
+        this.lineStyle(4, 0xe77c0c, 1)
+        for (let i = 0; i < p.length; i += 2) {
+            this.drawPolygon(center[0], center[1], p[i], p[i + 1])
+        }
+
+        this.lineStyle(3, 0x0aef43, 1)
+        for (let i = 0; i < p.length; i += 2) {
+            this.drawPolygon(center[0], center[1], p[i] + Math.cos(i / 10) * 6, p[i + 1] + Math.sin(i / 10) * 3)
+        }
+
+    }
+
+    drawSquiggleGeneral() {
+        this.pointsS = this.resampleByPoints(15)
+        const p = this.pointsS        
+        const n = p.length
+
+        // this.lineStyle(10, 0x8a8a8a, 1)
+        this.lineStyle(10, 0xe77c0c, 1)
+        for (let i = 0; i < p.length; i += 2) {
+            const curr = [p[i], p[i + 1]]
+            const next = [p[(i + 2) % n], p[(i + 3) % n]]
+
+            let offset = Math.round(Math.sin(i / 20) * (n / 2)) + n
+            offset += (offset % 2)
+            // let offset = 2
+                        
+            const after = [p[(i + offset) % n], p[(i + offset + 1) % n]]
+            this.drawPolygon(curr[0] + 7, curr[1] + 4, after[0] + 7, after[1] + 4)
+            this.endFill()
+        }
+
+        // this.lineStyle(8, 0xffffff, 1)
+        this.lineStyle(8, 0x0aef43, 1)        
+        for (let i = 0; i < p.length; i += 2) {
+            const curr = [p[i], p[i + 1]]
+            const next = [p[(i + 2) % n], p[(i + 3) % n]]
+
+            let offset = Math.round(Math.sin(i / 20) * (n / 2)) + n
+            offset += (offset % 2)
+            // let offset = 2
+
+            const after = [p[(i + offset) % n], p[(i + offset + 1) % n]]
+            this.drawPolygon(curr[0], curr[1], after[0], after[1])
+            this.endFill()
+        }
+    }
+
+    drawSquiggleOutline() {        
+        this.pointsS = this.resampleByPoints(12)
+        const p = this.pointsS        
+        const n = p.length
+
+        let polygon = []    
+        //polygon.push(p[0])
+        // polygon.push(p[1])
+
+        for (let i = 0; i < p.length; i += 2) {
+            const curr = [p[i], p[i + 1]]
+            const next = [p[(i + 2) % n], p[(i + 3) % n]]
+            const r = this.pRandom[i]
+
+            const center = geometric.lineInterpolate([curr, next])(r.posAlpha)            
+            const angle = Math.atan2(next[1] - curr[1], next[0] - curr[0])
+            const angle1 = angle + r.angle
+            const angle2 = angle - r.angle
+
+            const d = distanceAndAngleBetweenTwoPoints(curr[0], curr[1], next[0], next[1])
+            const radius1 = r.r1Factor * d.distance
+            const radius2 = r.r2Factor * d.distance
+
+            const pUnder = [
+                center[0] + radius1 * Math.cos(angle1),
+                center[1] + radius1 * Math.sin(angle1)
+            ]
+            const pOver = [
+                center[0] + radius2 * Math.cos(angle2),
+                center[1] + radius2 * Math.sin(angle2)
+            ]
+
+            polygon.push(next[0])
+            polygon.push(next[1])
+            polygon.push(pUnder[0])
+            polygon.push(pUnder[1])
+            polygon.push(pOver[0])
+            polygon.push(pOver[1])
+            
+            // this.drawPolygon(curr[0], curr[1], after[0], after[1])
+            // this.endFill()
+        }
+
+        this.lineStyle(8, 0x0aef43, 1)
+        this.drawPolygon(polygon)
+    }
+
+    drawBubblyOutline() {
+        this.pointsS = this.resampleByPoints(12)
+        const p = this.pointsS        
+        const n = p.length
+
+        let polygon = []    
+
+        this.lineStyle(8, 0x0aef43, 1)
+        this.moveTo(p[0], p[1])
+
+        for (let i = 0; i < p.length; i += 2) {
+            const curr = [p[i], p[i + 1]]
+            const next = [p[(i + 2) % n], p[(i + 3) % n]]
+            const r = this.pRandom[i]
+
+            const center = geometric.lineInterpolate([curr, next])(r.posAlpha)            
+            const angle = Math.atan2(next[1] - curr[1], next[0] - curr[0])
+            const angle1 = angle + r.angle
+            const angle2 = angle - r.angle
+
+            const d = distanceAndAngleBetweenTwoPoints(curr[0], curr[1], next[0], next[1])
+            const radius1 = r.r1Factor * d.distance
+            const radius2 = r.r2Factor * d.distance
+
+            const pUnder = [
+                center[0] + radius1 * Math.cos(angle1),
+                center[1] + radius1 * Math.sin(angle1)
+            ]
+            const pOver = [
+                center[0] + radius2 * Math.cos(angle2),
+                center[1] + radius2 * Math.sin(angle2)
+            ]
+
+            this.bezierCurveTo(pOver[0], pOver[1], pUnder[0], pUnder[1], next[0], next[1])            
+        }
+        
+        // this.drawPolygon(polygon)        
+        this.closePath()
     }
 
     drawDebug() {
